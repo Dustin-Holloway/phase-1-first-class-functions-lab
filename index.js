3109a5f66828ae19548a0e059ec9e17f7294673a
Dustin <dustinhollowaypdx@gmail.com>
2023-03-21 14:01:04 -0700

    first class fuctions commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const names = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = function (array) {
+  return array.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (array) {
+  return array.slice(-2);
+};
+
+function createFareMultiplier(int) {
+  return function fare(num) {
+    return num * int;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function selectDifferentDrivers(array, cbFunction) {
+  const result = cbFunction(array);
+  return result;
+}
